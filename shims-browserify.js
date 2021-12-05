2a348aa70f4828f079bc7d773c968718c16e9f22
Lirim Krosa <lirimkrosa@Lirims-MacBook-Pro.local>
2021-12-05 22:15:23 +0100

    Package updates

--- shims-browserify.js
+++ shims-browserify.js
@@ -27,6 +27,6 @@ module.exports = {
   "url": "~0.10.1",
   "util": "~0.10.1",
   "utp": "0.0.8",
-  "react-native-tcp": "^3.2.1",
+  "react-native-tcp-socket": "^5.5.0",
   "vm-browserify": "~0.0.1"
 }
